2a663fcaa85f713585981d65ec29073658ae0374
lb42 <lou.burnard@retired.ox.ac.uk>
2015-08-30 16:35:25 +0100

    Update purification scripts

--- Scripts/purify.sh
+++ Scripts/purify.sh
@@ -1,2 +1,5 @@
+mkdir Source/impureSpecs
+cp Source/Specs Source/impureSpecs
+rm Source/impureSpecs/data.* Source/impureSpecs/teidata.*
 cd Source/impureSpecs/
 for f in *.xml; do echo $f; saxon -o:../Specs/$f -s:$f -xsl:../../Scripts/purify.xsl; done
